2956864d02b8c287eff1813087d5ed1658fa746d
YUN-JIE929 <a0952482572@gamil.com>
2026-04-28 11:49:01 +0800

    最大值

--- myJS.js
+++ myJS.js
@@ -1,5 +1,14 @@
+var number=[1,5,2,8,3];
 
-var name='最棒的';
-var age=19;
+var max =-99999;
 
-console.log("我是"+name+".今年"+age+"歲.我想回家睡覺");
+
+for(var i=0;i<number.length;i++){
+    if(max < number[i]){
+        max = number[i];
+    }
+}
+
+console.log("最大值:"+max);
+
+console.log(number);
